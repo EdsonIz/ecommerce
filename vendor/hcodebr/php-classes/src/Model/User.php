1dd9c5b78bc2a5ad0bc85aece182d739b9b6ca8b
EdsonIz <edson@imagemfolheados.com.br>
2018-10-08 18:33:52 -0300

    CRUD dos usuários

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -21,7 +21,6 @@ class User extends Model {
         }
         
         $data = $results[0];
-        //var_dump($data);
         
         if (password_verify($password, $data["despassword"])){
             $user = new User();
@@ -44,13 +43,68 @@ class User extends Model {
                 (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin
            )
         {
-               header("LOCATION: /admin/login/");
+               header("LOCATION: /admin/login");
                exit();
         }
     }
     
     public static function logout(){
         $_SESSION[User::SESSION] = null;
+    }
+    
+    public static function listAll(){
+        $sql = new Sql();
+        
+        return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) ORDER BY b.desperson");
+    }
+    
+    public function save(){
+        $sql = new Sql();
+        
+        $results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":desperson"=>$this->getdesperson(),
+            ":deslogin"=>$this->getdeslogin(),
+            ":despassword"=>$this->getdespassword(),
+            ":desemail"=>$this->getdesemail(),
+            ":nrphone"=>$this->getnrphone(),
+            ":inadmin"=>$this->getinadmin()
+        ));
+        
+        $this->setData($results[0]);
+    }
+    
+    public function get($iduser){
+        $sql = new Sql();
+        
+        $results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE iduser = :iduser", array(
+            ":iduser"=>$iduser
+        ));
+        
+        $this->setData($results[0]);
+    }
+    
+    public function update(){
+        $sql = new Sql();
+        
+        $results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+            ":iduser"=>$this->getiduser(),
+            ":desperson"=>$this->getdesperson(),
+            ":deslogin"=>$this->getdeslogin(),
+            ":despassword"=>$this->getdespassword(),
+            ":desemail"=>$this->getdesemail(),
+            ":nrphone"=>$this->getnrphone(),
+            ":inadmin"=>$this->getinadmin()
+        ));
+        
+        $this->setData($results[0]);
+    }
+    
+    public function delete(){
+        $sql = new Sql();
+        
+        $sql->query("CALL sp_users_delete(:iduser)", array(
+            ":iduser"=>$this->getiduser()
+        ));
     }
     
 }
